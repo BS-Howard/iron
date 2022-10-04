60cf55f255e7af5aa47cdb7f90ab914f85ac34d2
BS-Howard <83260386+BS-Howard@users.noreply.github.com>
2022-10-04 13:53:11 +0800

    Update batch_home.ps1

--- batch_home.ps1
+++ batch_home.ps1
@@ -1,4 +1,4 @@
 while($true){
     Invoke-RestMethod -Uri "https://ithelp.ithome.com.tw/users/20136443/ironman/5310?page=1" -Method 'GET' -Headers $headers
-    timeout /t 30
+    Start-Sleep -Seconds 5
 }
